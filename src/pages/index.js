d4a64e2d41670b2a8e1930a0e10efa2c5261b501
CaroCodingK <130165356+carocodingk@users.noreply.github.com>
2024-06-05 15:42:27 -0400

    Added constant values for time and component to show list of tasks and their details

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,45 @@
 import { useState } from "react";
 
+// Initial values
+const workTime = 5;
+const breakTime = 2;
+const initialTasks = [{key: 0, taskName: 'Meal Prep', expectedCycles: 3, actualTime: 0, finished: false}, 
+                      {key: 1, taskName: 'Grocery shopping', expectedCycles: 2, actualTime: 0, finished: false} ];
 
-// function ActivityManager(){
-//   return(
-//     <div></div>
-//   );
-// }
+
+
+
+function TaskManager(){
+  const [taskList, setTaskList] = useState(initialTasks)
+
+  return(
+    <div>
+      <ul>
+        {taskList.map((task) => (
+          <li>
+            <input type="checkbox" checked={task.finished} />
+            <input type="button" value={task.taskName}/>
+            #### {task.expectedCycles} 
+            #### {task.actualTime}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 
 export default function Home() {
+  const [taskKeys, setTaskKey] = useState(2)
   const [taskInProgress, setTaskInProgress] = useState({
     taskId: -1,
     taskName: ""
   })
-  
+
 
   return (
     <div>
-
+      <TaskManager />
     </div>
   );
 }
