e50a12fdc515478125d6982392bf36eaa8f61b02
CaroCodingK <130165356+carocodingk@users.noreply.github.com>
2024-06-05 23:29:32 -0400

    Added function that stores the task that has been chosen

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,22 +3,35 @@ import { useState } from "react";
 // Initial values
 const workTime = 5;
 const breakTime = 2;
-const initialTasks = [{key: 0, taskName: 'Meal Prep', expectedCycles: 3, actualTime: 0, finished: false}, 
+const initialTasks = [{key: 0, taskName: 'Meal Prep', expectedCycles: 3, actualTime: 0, finished: true}, 
                       {key: 1, taskName: 'Grocery shopping', expectedCycles: 2, actualTime: 0, finished: false} ];
 
 
-function TaskManager(){
+function TaskManager({setTaskInProgress}){
   const [taskKeys, setTaskKey] = useState(2)
   const [taskList, setTaskList] = useState(initialTasks)
-
   console.log(taskList)
+
+  const chooseTask = (task) => {
+    if (!task.finished){
+      setTaskInProgress({
+        key: task.key,
+        taskName: task.taskName
+      })
+      // console.log(task.taskName)
+    }
+    else{
+      console.log("This task has been resolved")
+    }
+  }
+
   return(
     <div>
       <ul>
         {taskList.map((task) => (
           <li>
             <input type="checkbox" checked={task.finished} />
-            <input type="button" value={task.taskName}/>
+            <input type="button" value={task.taskName} onClick={()=>chooseTask(task)}/>
             #### {task.expectedCycles} 
             #### {task.actualTime}
           </li>
@@ -35,8 +48,8 @@ function NewTask({taskKeys, setTaskKey, setTaskList}){
     expectedCycles: "", 
   })
 
-  // Verifies if the input entered by user are valid: task description is not empty and 
-  //assigns a default of 1 to expected number of cycles when not specified
+  //  Verifies if the input entered by user are valid: task description is not empty and 
+  //  assigns a default of 1 to expected number of cycles when not specified
   const inputVerification = () => {
     console.log(newTask)
     if (newTask.taskName.length > 0){
@@ -78,7 +91,6 @@ function NewTask({taskKeys, setTaskKey, setTaskList}){
 
   return(
     <div className="flexBox flexRow">
-
       {/* Task description field */}
       <div className="flexBox flexColumn">
         <label>Task description: </label>
@@ -92,7 +104,6 @@ function NewTask({taskKeys, setTaskKey, setTaskList}){
           value={newTask.taskName}
         />
       </div>
-
       {/* Task expected cycles field */}
       <div className="flexBox flexColumn">
         <label>Expected Cycles:</label>
@@ -106,14 +117,12 @@ function NewTask({taskKeys, setTaskKey, setTaskList}){
           value={newTask.expectedCycles}
         />
       </div>
-
       {/* Add to list button */}
       <input type="button" value="+" onClick={()=> {  
         inputVerification()
         console.log("submit new task")
       }
-      }/>
-             
+      }/>      
     </div>
     
   );
@@ -125,10 +134,11 @@ export default function Home() {
     taskName: ""
   })
 
+  console.log(taskInProgress)
 
   return (
     <div>
-      <TaskManager />
+      <TaskManager setTaskInProgress={setTaskInProgress} />
     </div>
   );
 }
