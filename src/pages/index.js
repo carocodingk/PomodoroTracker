95d215b618980f7a27d49c7c40357027690df37f
CaroCodingK <130165356+carocodingk@users.noreply.github.com>
2024-06-10 21:51:46 -0400

    Moved pomodoro timer component to its own module to improve readability

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,128 +1,128 @@
 import { useEffect, useState } from "react";
-// import Testing from "./edit.js";
-// import EditMenu from "./api/editMenu.js";
+
+import PomodoroTimer from "./pomodoroTimer.js";
 import DialogBox from "./dialogBox.js";
 
 // Initial values
-const workTime = 5;
-const breakTime = 2;
+// const workTime = 5;
+// const breakTime = 2;
 const initialTasks =  [{key: 0, taskName: 'Meal Prep', expectedCycles: 3, actualTime: 3700, finished: false}, 
                       {key: 1, taskName: 'Grocery shopping', expectedCycles: 2, actualTime: 0, finished: false},
                       {key: 2, taskName: 'Grocery shopping1', expectedCycles: 2, actualTime: 0, finished: false},
                       {key: 3, taskName: 'Grocery shopping2', expectedCycles: 2, actualTime: 0, finished: false},  
                     ];
 
-function PomodoroTimer({taskInProgress, setCycleComplete}){
-  const [workTimer, setWorkTimer] = useState(workTime);
-  const [workTimerRun, setWorkTimerRun] = useState(false);
-  const [breakTimer, setBreakTimer] = useState(breakTime);
-  const [breakTimerRun, setBreakTimerRun] = useState(false);
-  const [timesUp, setTimesUp] = useState(false);
+// function PomodoroTimer({taskInProgress, setCycleComplete}){
+//   const [workTimer, setWorkTimer] = useState(workTime);
+//   const [workTimerRun, setWorkTimerRun] = useState(false);
+//   const [breakTimer, setBreakTimer] = useState(breakTime);
+//   const [breakTimerRun, setBreakTimerRun] = useState(false);
+//   const [timesUp, setTimesUp] = useState(false);
 
-  const startTimer = (taskInProgress) => {
-    setCycleComplete(false) //Restart the cycle
-    if (taskInProgress.key != -1){
-      setWorkTimerRun(!workTimerRun)
-    }
-    else
-      console.log("Please select a task to work on! ")
-  }
+//   const startTimer = (taskInProgress) => {
+//     setCycleComplete(false) //Restart the cycle
+//     if (taskInProgress.key != -1){
+//       setWorkTimerRun(!workTimerRun)
+//     }
+//     else
+//       console.log("Please select a task to work on! ")
+//   }
 
-  const resetTimer = () => {
-    setWorkTimerRun(false)
-    setBreakTimerRun(false)
-    setWorkTimer(workTime)
-    setBreakTimer(breakTime)
-  }
+//   const resetTimer = () => {
+//     setWorkTimerRun(false)
+//     setBreakTimerRun(false)
+//     setWorkTimer(workTime)
+//     setBreakTimer(breakTime)
+//   }
 
-  const alarmAlert = () => {
-    return(
-      <div className="flexBox flexColumn flexJustifyCenter">
-        <h2 className="centerText">Your working time is up!</h2>
-        <h2 className="centerText">Let's take a break</h2>
-        <input className="lightBackground okButton timerButton hoverItem" type="button" value="OK" 
-          onClick={()=> setTimesUp(false)}
-        />
-      </div>
-    );
-  }
+//   const alarmAlert = () => {
+//     return(
+//       <div className="flexBox flexColumn flexJustifyCenter">
+//         <h2 className="centerText">Your working time is up!</h2>
+//         <h2 className="centerText">Let's take a break</h2>
+//         <input className="lightBackground okButton timerButton hoverItem" type="button" value="OK" 
+//           onClick={()=> setTimesUp(false)}
+//         />
+//       </div>
+//     );
+//   }
 
 
-  useEffect(() => {
-    let timer1, timer2
-    if (workTimerRun){
-      timer1 = setTimeout(() => {
-        setWorkTimer((prevTime) => prevTime-1)
-      }, 1000);
-      if (workTimer === 0){ //time's up
-        setWorkTimerRun(false) //Stop work timer
-        setBreakTimerRun(true) //Start break timer
-        setTimesUp(true)
-      }
-    }
+//   useEffect(() => {
+//     let timer1, timer2
+//     if (workTimerRun){
+//       timer1 = setTimeout(() => {
+//         setWorkTimer((prevTime) => prevTime-1)
+//       }, 1000);
+//       if (workTimer === 0){ //time's up
+//         setWorkTimerRun(false) //Stop work timer
+//         setBreakTimerRun(true) //Start break timer
+//         setTimesUp(true)
+//       }
+//     }
     
-    if (breakTimerRun){
-      timer2 = setTimeout(() => {
-        setBreakTimer((prevTime) => prevTime-1)
-      }, 1000);
-      if (breakTimer === 0){
-        setBreakTimerRun(false) //Stop break timer
-        setCycleComplete(true)  //Indicates a pomodoro cycle (work + break) is complete
-        resetTimer()
-      }
-    }
+//     if (breakTimerRun){
+//       timer2 = setTimeout(() => {
+//         setBreakTimer((prevTime) => prevTime-1)
+//       }, 1000);
+//       if (breakTimer === 0){
+//         setBreakTimerRun(false) //Stop break timer
+//         setCycleComplete(true)  //Indicates a pomodoro cycle (work + break) is complete
+//         resetTimer()
+//       }
+//     }
 
-    return() => {
-      if (timer1)
-        clearTimeout(timer1)
-      else if (timer2)
-        clearTimeout(timer2)
-    }
+//     return() => {
+//       if (timer1)
+//         clearTimeout(timer1)
+//       else if (timer2)
+//         clearTimeout(timer2)
+//     }
 
-  },[workTimer, workTimerRun, breakTimer, breakTimerRun]);
+//   },[workTimer, workTimerRun, breakTimer, breakTimerRun]);
 
-  const secondsW = workTimer % 60
-  const minutesW = Math.floor(workTimer / 60)
-  const secondsB = breakTimer % 60
-  const minutesB = Math.floor(breakTimer / 60)
+//   const secondsW = workTimer % 60
+//   const minutesW = Math.floor(workTimer / 60)
+//   const secondsB = breakTimer % 60
+//   const minutesB = Math.floor(breakTimer / 60)
 
-  return(
-    <div>
-      {timesUp && <DialogBox infoField={alarmAlert} />}
-    <div className="yellowBox">
-      <div>
-        <p className="centerText">{taskInProgress.taskName}</p>
-        <div>
-          <p className="orange centerText">Work Time</p>
-          <p className="centerText brown font1">{minutesW}:{secondsW < 10? `0${secondsW}` : secondsW}</p>
-        </div>
-        <div>
-          <p className="orange centerText">Break Time</p>
-          <p className="centerText brown font1">{minutesB}:{secondsB < 10? `0${secondsB}` : secondsB}</p>
-        </div>
-        <div className="centerMargin">
-        <div className="flexBox flexJustifyCenter">
-          <input className="lightBackground timerButton brown hoverItem" 
-            type="button" 
-            value={workTimerRun || breakTimerRun? 'PAUSE':'START'} 
-            onClick={() => 
-              startTimer(taskInProgress)
-            } 
-          />
-          <input className="lightBackground timerButton brown hoverItem" 
-            type="button" 
-            value='RESET' 
-            onClick={() => 
-              resetTimer()
-            } 
-          />
-        </div>
-        </div>
-      </div>
-    </div>
-    </div>
-  );
-}
+//   return(
+//     <div>
+//       {timesUp && <DialogBox infoField={alarmAlert} />}
+//     <div className="yellowBox">
+//       <div>
+//         <p className="centerText">{taskInProgress.taskName}</p>
+//         <div>
+//           <p className="orange centerText">Work Time</p>
+//           <p className="centerText brown font1">{minutesW}:{secondsW < 10? `0${secondsW}` : secondsW}</p>
+//         </div>
+//         <div>
+//           <p className="orange centerText">Break Time</p>
+//           <p className="centerText brown font1">{minutesB}:{secondsB < 10? `0${secondsB}` : secondsB}</p>
+//         </div>
+//         <div className="centerMargin">
+//         <div className="flexBox flexJustifyCenter">
+//           <input className="lightBackground timerButton brown hoverItem" 
+//             type="button" 
+//             value={workTimerRun || breakTimerRun? 'PAUSE':'START'} 
+//             onClick={() => 
+//               startTimer(taskInProgress)
+//             } 
+//           />
+//           <input className="lightBackground timerButton brown hoverItem" 
+//             type="button" 
+//             value='RESET' 
+//             onClick={() => 
+//               resetTimer()
+//             } 
+//           />
+//         </div>
+//         </div>
+//       </div>
+//     </div>
+//     </div>
+//   );
+// }
 
 
 function TaskManager({taskInProgress, setTaskInProgress, cycleComplete, setCycleComplete}){
---
+++ src/pages/pomodoroTimer.js
@@ -0,0 +1,118 @@
+import { useEffect, useState } from "react";
+
+const workTime = 5;
+const breakTime = 2;
+
+function PomodoroTimer({taskInProgress, setCycleComplete}){
+    const [workTimer, setWorkTimer] = useState(workTime);
+    const [workTimerRun, setWorkTimerRun] = useState(false);
+    const [breakTimer, setBreakTimer] = useState(breakTime);
+    const [breakTimerRun, setBreakTimerRun] = useState(false);
+    const [timesUp, setTimesUp] = useState(false);
+  
+    const startTimer = (taskInProgress) => {
+      setCycleComplete(false) //Restart the cycle
+      if (taskInProgress.key != -1){
+        setWorkTimerRun(!workTimerRun)
+      }
+      else
+        console.log("Please select a task to work on! ")
+    }
+  
+    const resetTimer = () => {
+      setWorkTimerRun(false)
+      setBreakTimerRun(false)
+      setWorkTimer(workTime)
+      setBreakTimer(breakTime)
+    }
+  
+    const alarmAlert = () => {
+      return(
+        <div className="flexBox flexColumn flexJustifyCenter">
+          <h2 className="centerText">Your working time is up!</h2>
+          <h2 className="centerText">Let's take a break</h2>
+          <input className="lightBackground okButton timerButton hoverItem" type="button" value="OK" 
+            onClick={()=> setTimesUp(false)}
+          />
+        </div>
+      );
+    }
+  
+  
+    useEffect(() => {
+      let timer1, timer2
+      if (workTimerRun){
+        timer1 = setTimeout(() => {
+          setWorkTimer((prevTime) => prevTime-1)
+        }, 1000);
+        if (workTimer === 0){ //time's up
+          setWorkTimerRun(false) //Stop work timer
+          setBreakTimerRun(true) //Start break timer
+          setTimesUp(true)
+        }
+      }
+      
+      if (breakTimerRun){
+        timer2 = setTimeout(() => {
+          setBreakTimer((prevTime) => prevTime-1)
+        }, 1000);
+        if (breakTimer === 0){
+          setBreakTimerRun(false) //Stop break timer
+          setCycleComplete(true)  //Indicates a pomodoro cycle (work + break) is complete
+          resetTimer()
+        }
+      }
+  
+      return() => {
+        if (timer1)
+          clearTimeout(timer1)
+        else if (timer2)
+          clearTimeout(timer2)
+      }
+  
+    },[workTimer, workTimerRun, breakTimer, breakTimerRun]);
+  
+    const secondsW = workTimer % 60
+    const minutesW = Math.floor(workTimer / 60)
+    const secondsB = breakTimer % 60
+    const minutesB = Math.floor(breakTimer / 60)
+  
+    return(
+      <div>
+        {timesUp && <DialogBox infoField={alarmAlert} />}
+      <div className="yellowBox">
+        <div>
+          <p className="centerText">{taskInProgress.taskName}</p>
+          <div>
+            <p className="orange centerText">Work Time</p>
+            <p className="centerText brown font1">{minutesW}:{secondsW < 10? `0${secondsW}` : secondsW}</p>
+          </div>
+          <div>
+            <p className="orange centerText">Break Time</p>
+            <p className="centerText brown font1">{minutesB}:{secondsB < 10? `0${secondsB}` : secondsB}</p>
+          </div>
+          <div className="centerMargin">
+          <div className="flexBox flexJustifyCenter">
+            <input className="lightBackground timerButton brown hoverItem" 
+              type="button" 
+              value={workTimerRun || breakTimerRun? 'PAUSE':'START'} 
+              onClick={() => 
+                startTimer(taskInProgress)
+              } 
+            />
+            <input className="lightBackground timerButton brown hoverItem" 
+              type="button" 
+              value='RESET' 
+              onClick={() => 
+                resetTimer()
+              } 
+            />
+          </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    );
+  }
+
+  export default PomodoroTimer;
