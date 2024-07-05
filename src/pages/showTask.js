2fcb869f4fcab383cfea7a795ab0cd631d1290cf
CaroCodingK <130165356+carocodingk@users.noreply.github.com>
2024-07-04 23:09:02 -0400

    Added save changes and cancel edition functions

--- src/pages/showTask.js
+++ src/pages/showTask.js
@@ -1,20 +1,81 @@
-function ShowTask ({taskList, taskToEdit, showDetails}){
-    console.log('inside show task')
-    console.log('show details ', showDetails )
+import { useState } from "react"
+
+function ShowTask ({taskList, setTaskList, taskToEdit, showDetails, setShowDetails}){
+    // console.log('inside show task')
+    // console.log('show details ', showDetails )
+    const [editedTask, setEditedTask] = useState({
+        key: taskToEdit.key, 
+        taskName: taskToEdit.taskName, 
+        expectedCycles: taskToEdit.expectedCycles, 
+        actualTime: taskToEdit.actualTime, 
+        finished: taskToEdit.finished
+    })
+
+    const savingChanges = (idx) => {
+        // console.log("inside saving changes")
+        const newEditedList = taskList.toSpliced(idx, 1, editedTask)
+        setTaskList(newEditedList)
+        setShowDetails(false)
+    }
+
+    // console.log(taskToEdit)
+
     return(
         <div>
-            {taskList.map((task) => {
+            {taskList.map((task, idx) => {
                 if (task.key === taskToEdit.key){
                     return(
-                        <div id="showTaskBackground">
-                            <div id="test" className="flexBox flexColumn">
-                                <input type="text" value={task.taskName} />
-                                <input type="text" value={task.expectedCycles} />
-                                <input type="text" value={task.actualTime} />
-                                <input type="checkbox" value={task.finished} />
+                        <div id="taskVerification1">
+                            <div id="test" className="flexBox flexColumn taskVerification2">
                                 <div>
-                                    <input type="button" value="Save" />
-                                    <input type="button" value="Cancel" />
+                                    <label>Task:</label>
+                                    <input type="text" value={editedTask.taskName} onChange={
+                                        (e) => 
+                                            setEditedTask((prevState) => ({
+                                                ...prevState,
+                                                taskName: e.target.value
+                                            })
+                                        )} 
+                                    />
+                                </div>
+                                {/* <div className="flexBox"> */}
+                                    <div>
+                                        <label>Expected Cycles:</label>
+                                        <input type="text" value={editedTask.expectedCycles} onChange={
+                                            (e) => 
+                                                setEditedTask((prevState) => ({
+                                                    ...prevState,
+                                                    expectedCycles: e.target.value
+                                                })
+                                            )} 
+                                        />
+                                    </div>
+                                    <div>
+                                        <label>Actual Time:</label>
+                                        <input type="text" value={editedTask.actualTime} onChange={
+                                            (e) => 
+                                                setEditedTask((prevState) => ({
+                                                    ...prevState,
+                                                    actualTime: e.target.value
+                                                })
+                                            )} 
+                                        />
+                                    </div>
+                                    <div>
+                                        <label>Finished?</label>
+                                        <input type="checkbox" value={editedTask.finished} onChange={
+                                            (e) => 
+                                                setEditedTask((prevState) => ({
+                                                    ...prevState,
+                                                    finished: e.target.value
+                                                })
+                                            )} 
+                                        />
+                                    </div>
+                                {/* </div> */}
+                                <div>
+                                    <input type="button" value="Save" onClick={() => savingChanges(idx)} />
+                                    <input type="button" value="Cancel" onClick={() => setShowDetails(false)} />
                                 </div>
                             </div>
                         </div>
