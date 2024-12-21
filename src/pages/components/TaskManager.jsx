45bedca8a610fc65242d16b66a17f0ab2904da6c
Supermoon <supermoon@Caros-MacBook-Pro.local>
2024-12-21 11:31:57 -0500

    Added a menu button for each task that opens one at a time. In the menu, added option to delete the task from the list

--- src/pages/components/TaskManager.jsx
+++ src/pages/components/TaskManager.jsx
@@ -13,6 +13,9 @@ function TaskManager({cycleComplete, setCycleComplete}){
   const t = useContext(UserContext)
   const [nextTaskKey, setNextTaskKey] = useState(4)
   const [taskList, setTaskList] = useState(initialTasks)
+  const [showMenu, setShowMenu] = useState(false)
+  const [menuRequest, setMenuRequest] = useState(-1) //nobody has requested the menu
+  const [showMenuKey, setShowMenuKey] = useState(-1) //keeps track what task enters edition mode
   // const [ch, setCh] = useState(null)
 
   const selectTask = (task) => {
@@ -38,18 +41,15 @@ function TaskManager({cycleComplete, setCycleComplete}){
     );
   }
 
-  // const taskCompletion = () => {
-  //   return(
-  //     <div>
-  //       <h3>Has the task been completed?</h3>
-  //       <div>
-  //         <input type='button' value={'YES!'} onClick={()=>console.log('yes!')} />
-  //         <input type='button' value={'Not yet!'} onClick={()=>console.log('No!')} />
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  const deleteTask = (key) => {
+    let taskListUpdate = taskList.filter((task) => (task.key !== key))
+    console.log('tasklistupdate for delete: ',taskListUpdate)
+  }
 
+  const openMenu = (key) => {
+    setMenuRequest(key)
+    console.log('Menu requested by: ', key)
+  }
 
   return(
     <div>
@@ -61,12 +61,22 @@ function TaskManager({cycleComplete, setCycleComplete}){
                 <input type='button' value={task.taskName} onClick={()=> selectTask(task)} />
                 <p>{task.expectedCycles}</p>
                 <p>{timeProcessing(task.actualTime)}</p>
+                <div>
+                  <input type='button' value='More' onClick={()=>openMenu(task.key)} />
+                  {menuRequest === task.key? 
+                    <div>
+                      <input type="button" value='Edit' onClick={()=>console.log('edit')}/>
+                      <input type="button" value='Delete' onClick={()=>deleteTask(task.key)}/>
+                    </div>
+                    :
+                    null
+                  }
+                </div>
               </div>
           </li>
         )}
       </ul>
       <NewTaskBar nextTaskKey={nextTaskKey} setNextTaskKey={setNextTaskKey} setTaskList={setTaskList} />
-      {/* {console.log('###### ', taskList)} */}
       {cycleComplete && 
       <TaskCompletion
         taskList={taskList}
@@ -78,80 +88,5 @@ function TaskManager({cycleComplete, setCycleComplete}){
     </div>
   ) 
 }
-// function TaskManager({taskInProgress, setTaskInProgress, cycleComplete, setCycleComplete}){
-//   const [taskKeys, setTaskKey] = useState(4)
-//   const [taskList, setTaskList] = useState(initialTasks)
-
-//   const chooseTask = (task) => {
-//     if (!task.finished){
-//       setTaskInProgress({
-//         key: task.key,
-//         taskName: task.taskName
-//       })
-//     }
-//     else{
-//       console.log("This task has been resolved")
-//     }
-//     return('heeeelooo');
-//   }
-
-//   const timeProcessing = (time) => {
-//     const secondsW = time % 60
-//     const minutesW = Math.floor(time / 60)
-//     const output = `${minutesW}:${secondsW < 10? `0${secondsW}` : secondsW}`
-    
-//     return(
-//       output
-//     );
-//   }
-
-
-
-//   return(
-//     <div>
-//       <div id="colName" className="flexBox justifySpaceBetween">
-//         <div id="colDescription" className="flexBox">
-//           <p id="columnLeft" className="columns">Task description</p>
-//         </div>
-//         <div>
-//           <p className="columns rightAlign details">Expected cycles</p>
-//           <p className="columns rightAlign details">Current time</p>
-//         </div>
-//       </div>
-//       <ul id="taskList">
-//         {taskList.map((task) => (
-//           <li className="flexBox flexAlignCenter taskItem justifySpaceBetween">
-//             <div>            
-//               <input className="checkBox" type="checkbox" checked={task.finished} />
-//               <input id="taskName" className="lightBackground brown hoverItem" 
-//                 type="button" 
-//                 value={task.taskName} 
-//                 onClick={()=>chooseTask(task)}
-//               />
-//             </div>
-//             <div>
-//               <p  className="brown details rightAlign">{task.expectedCycles}</p>
-//               <p id="totalTime" className="brown details rightAlign">{timeProcessing(task.actualTime)}</p>
-//             </div>
-//           </li>
-//         ))}
-//       </ul>
-//       <div id="bottom">
-//         <NewTask 
-//           taskKeys={taskKeys} 
-//           setTaskKey={setTaskKey} 
-//           setTaskList={setTaskList} 
-//         />
-//       </div>
-//       {cycleComplete && 
-//       <TaskCompletion 
-//         taskInProgress={taskInProgress} 
-//         setTaskInProgress={setTaskInProgress} 
-//         taskList={taskList} setTaskList={setTaskList} 
-//         setCycleComplete={setCycleComplete} 
-//       />}
-//     </div>
-//   );
-// }
 
 export default TaskManager;
