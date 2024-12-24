a28df27cf396535feccf3a50f3c6a7f15a9e93d9
Supermoon <supermoon@Caros-MacBook-Pro.local>
2024-12-24 16:47:37 -0500

    Updated styling

--- src/pages/components/AlarmAlert.jsx
+++ src/pages/components/AlarmAlert.jsx
@@ -1,9 +1,11 @@
 function AlarmAlert({setTimesUp}){
   return(
-    <div className="flexBox flexColumn flexJustifyCenter">
-      <h2 className="centerTex test">Your working time is up!</h2>
-      <h2 className="centerText test">Let's take a break</h2>
-      <input className="okButton" type="button" value="OK" onClick={()=> setTimesUp(false)} />
+    <div className="posAbsoluteTop flexBox flexColumn flexJustifyCenter modalBox">
+      <div id="timesUp1" className="flexBox flexColumn flexJustifySpaceAround dialogBox">
+        <h2 className="centerTex test">Your working time is up!</h2>
+        <h2 className="centerText test">Let's take a break</h2>
+        <input className="okButton" type="button" value="OK" onClick={()=> setTimesUp(false)} />
+      </div>
     </div>
   );
 }
--- src/pages/components/AppInstructions.jsx
+++ src/pages/components/AppInstructions.jsx
@@ -1,8 +1,8 @@
 function AppInstructions({setSeenInstructions}){
   return(
-    <div className="posAbsoluteTop flexBox flexJustifyCenter modalBox">
-      <div className="flexBox flexColumn flexJustifySpaceAround dialogBox">
-        <ol>
+    <div className="posAbsoluteTop flexBox flexColumn flexJustifyCenter modalBox">
+      <div className="flexBox flexColumn flexJustifySpaceAround1 dialogBox">
+        <ol id="list">
           <li>Welcome to a Pomodoro task tracker</li>
           <li>Each Pomodoro cycle consists of 10s of working time and 2s of resting time</li>
           <li>Enter the task and the number of expected pomodoro cycles it requires. Default cycle is 1</li>
@@ -14,7 +14,8 @@ function AppInstructions({setSeenInstructions}){
         <input className="timerButton hoverItem okButton"  
           type="button" 
           value="OK" 
-          onClick={()=> setSeenInstructions(true)} />
+          onClick={()=> setSeenInstructions(true)} 
+        />
       </div>
     </div>
   );
