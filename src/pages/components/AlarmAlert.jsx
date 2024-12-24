60a42921cb6415a4ee7e3afd5a6a45f4cecc3dc8
Supermoon <supermoon@Caros-MacBook-Pro.local>
2024-12-24 17:20:39 -0500

    Added styling tags to elements in the component

--- src/pages/components/AlarmAlert.jsx
+++ src/pages/components/AlarmAlert.jsx
@@ -4,7 +4,7 @@ function AlarmAlert({setTimesUp}){
       <div className="flexBox flexColumn dialogBox">
         <h3 className="boxTitle centerText">Your working time is up!</h3>
         <h3 className="boxTitle centerText">Let's take a break</h3>
-        <input className="okButton" type="button" value="OK" onClick={()=> setTimesUp(false)} />
+        <input className="timerButton hoverItem" type="button" value="OK" onClick={()=> setTimesUp(false)} />
       </div>
     </div>
   );
