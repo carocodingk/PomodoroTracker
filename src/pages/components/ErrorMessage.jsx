2b83e42fe258b48d225c09309a16a85384c7aaf5
Supermoon <supermoon@Calatheas-MacBook-Pro.local>
2025-04-18 20:22:38 -0400

    Fixed styling of error message component

--- src/pages/components/ErrorMessage.jsx
+++ src/pages/components/ErrorMessage.jsx
@@ -1,9 +1,11 @@
+import styles from "@/styles/popUpBox.module.css"
+
 function ErrorMessage({setShowErrorMsg}){
   return(
-    <div className="posAbsoluteTop flexBox flexColumn flexJustifyCenter modalBox">
-      <div id="errorBox" className="dialogBox flexBox flexColumn">
-        <h3 className="boxTitle centerText">Error! The task you are trying to enter is empty!</h3>
-        <input className="timerButton hoverItem okButton"
+    <div className={styles.modalBox}>
+      <div className={styles.dialogBox}>
+        <h3 className={styles.boxText}>Error! The task you are trying to enter is empty!</h3>
+        <input className={styles.okButton}
           type="button" value='OK!' onClick={() => setShowErrorMsg(false)} />
       </div>
     </div>
