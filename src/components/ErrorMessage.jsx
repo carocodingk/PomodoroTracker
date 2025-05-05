c97eb09beb2e2df5e872685f59b77c8969b23e66
CaroCodingK <130165356+carocodingk@users.noreply.github.com>
2025-05-05 17:54:15 -0400

    Changed classnames for Error message component to refer to styles.css

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,11 +1,9 @@
-import styles from "@/styles/popUpBox.module.css"
-
 function ErrorMessage({setShowErrorMsg}){
   return(
-    <div className={styles.modalBox}>
-      <div className={styles.dialogBox}>
-        <h3 className={styles.boxText}>Error! The task you are trying to enter is empty!</h3>
-        <input className={styles.okButton}
+    <div className='modalBox'>
+      <div className='dialogBox_small'>
+        <h3>Error! The task you are trying to enter is empty!</h3>
+        <input className='okButton'
           type="button" value='OK!' onClick={() => setShowErrorMsg(false)} />
       </div>
     </div>
