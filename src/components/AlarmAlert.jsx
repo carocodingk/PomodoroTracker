08b20426716f79c985b9ddad0b5e42265f7c9550
CaroCodingK <130165356+carocodingk@users.noreply.github.com>
2025-05-05 17:02:35 -0400

    Changed styling to refer to styles.css

--- src/components/AlarmAlert.jsx
+++ src/components/AlarmAlert.jsx
@@ -1,12 +1,12 @@
-import styles from "@/styles/popUpBox.module.css"
+// import styles from "@/styles/popUpBox.module.css"
 
 function AlarmAlert({setTimesUp}){
   return(
-    <div className={styles.modalBox}>
-      <div className={styles.dialogBox}>
-        <h3 className={styles.boxText}>Your working time is up!</h3>
-        <h3 className={styles.boxText}>Let's take a break</h3>
-        <input className={styles.okButton} type="button" value="OK" onClick={()=> setTimesUp(false)} />
+    <div className='modalBox'>
+      <div className='dialogBox_small'>
+        <h3 className='boxText'>Your working time is up!</h3>
+        <h3 className='boxText'>Let's take a break</h3>
+        <input className='okButton' type="button" value="OK" onClick={()=> setTimesUp(false)} />
       </div>
     </div>
   );
